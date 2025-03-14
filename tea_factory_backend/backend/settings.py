4c209c9c9886f952020dfc3ec55ca4c33d8e7cd5
Maunducyrus <maunducyrus123@gmail.com>
2025-03-14 16:46:08 +0300

    settings.py

--- tea_factory_backend/backend/settings.py
+++ tea_factory_backend/backend/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware', #Handles frontend-backend communication
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
