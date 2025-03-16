8f3a23e91a05c20533d4fd5781a8ee5bc6fe9951
Maunducyrus <maunducyrus123@gmail.com>
2025-03-16 20:08:22 +0300

    product model

--- tea_factory_backend/api/admin.py
+++ tea_factory_backend/api/admin.py
@@ -2,3 +2,8 @@ from django.contrib import admin
 from .models import Product, Farmer, Order, Report, UserProfile
 
 # Register your models here.
+# Register Product Model
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price_per_kg', 'stock_quantity', 'created_at')
+    search_fields = ('name',)
