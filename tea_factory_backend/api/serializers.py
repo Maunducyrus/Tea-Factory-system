644983f6771196e1f519f56cb114a63c6f4f533d
Maunducyrus <maunducyrus123@gmail.com>
2025-03-16 20:32:38 +0300

    product serializers

--- tea_factory_backend/api/serializers.py
+++ tea_factory_backend/api/serializers.py
@@ -1,2 +1,8 @@
 from rest_framework import serializers
 from .models import Product, Farmer, Order, Report, UserProfile
+
+# Product Serializer
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = '__all__'
