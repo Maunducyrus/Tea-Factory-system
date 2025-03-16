83fe642f1ac0b93ea8895fb2d7001223576a4acd
Maunducyrus <maunducyrus123@gmail.com>
2025-03-16 14:03:05 +0300

    class Order

--- tea_factory_backend/api/models.py
+++ tea_factory_backend/api/models.py
@@ -24,3 +24,26 @@ class Farmer(models.Model):
         return self.name
 
 # customer orders models
+class Order(models.Model):
+    STATUS_CHOICES = [
+        ('Pending', 'Pending'),
+        ('Processing', 'Processing'),
+        ('Delivered', 'Delivered'),
+        ('Cancelled', 'Cancelled'),
+    ]
+
+    customer_name = models.CharField(max_length=100)
+    phone_number = models.CharField(max_length=15)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity_kg = models.PositiveIntegerField()
+    total_price = models.DecimalField(max_digits=10, decimal_places=2, blank=True)
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='Pending')
+    order_date = models.DateTimeField(auto_now_add=True)
+
+    def save(self, *args, **kwargs):
+        # Calculate total price before saving
+        self.total_price = self.quantity_kg * self.product.price_per_kg
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return f"Order by {self.customer_name} - {self.product.name}"
