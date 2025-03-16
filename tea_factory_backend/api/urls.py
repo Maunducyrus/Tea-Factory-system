0c24f9bbe033c239bf4b514a9d59aebb6f1df5cb
Maunducyrus <maunducyrus123@gmail.com>
2025-03-16 21:03:08 +0300

    routers

--- tea_factory_backend/api/urls.py
+++ tea_factory_backend/api/urls.py
@@ -2,3 +2,9 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import ProductViewSet, FarmerViewSet, OrderViewSet, ReportViewSet, UserProfileViewSet, register_user, login_user, logout_user
 
+router = DefaultRouter()
+router.register(r'products', ProductViewSet)
+router.register(r'farmers', FarmerViewSet)
+router.register(r'orders', OrderViewSet)
+router.register(r'reports', ReportViewSet)
+router.register(r'users', UserProfileViewSet)
