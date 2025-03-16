9070017f64e328e0455c30ba44dcce61c5ff758c
Maunducyrus <maunducyrus123@gmail.com>
2025-03-16 13:59:14 +0300

    product model

--- tea_factory_backend/api/models.py
+++ tea_factory_backend/api/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+
+class Product(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    description = models.TextField()
+    price_per_kg = models.DecimalField(max_digits=10, decimal_places=2)
+    stock_quantity = models.PositiveIntegerField(default=0)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
