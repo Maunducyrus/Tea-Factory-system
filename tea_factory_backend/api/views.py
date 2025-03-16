3c91bac57e57b08e0bacd74ec6c68382fea1d5ee
Maunducyrus <maunducyrus123@gmail.com>
2025-03-16 20:44:38 +0300

    imports in views.py

--- tea_factory_backend/api/views.py
+++ tea_factory_backend/api/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from rest_framework import viewsets, permissions
+from .models import Product, Farmer, Order, Report, UserProfile
+from .serializers import ProductSerializer, FarmerSerializer, OrderSerializer, ReportSerializer, UserProfileSerializer
+from django.contrib.auth.models import User
+from rest_framework.response import Response
+from rest_framework.decorators import api_view, permission_classes
+from django.contrib.auth import authenticate
+from rest_framework.authtoken.models import Token
+from rest_framework.permissions import IsAuthenticated
 
 # Create your views here.
