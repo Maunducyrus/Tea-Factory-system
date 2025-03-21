ea7cf817e8c850956cbe5e31a68a31a7248454ff
Maunducyrus <maunducyrus123@gmail.com>
2025-03-21 19:44:47 +0300

    models

--- tea_factory_backend/api/models.py
+++ tea_factory_backend/api/models.py
@@ -39,7 +39,7 @@ class Order(models.Model):
     phone_number = models.CharField(max_length=15)
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     quantity_kg = models.PositiveIntegerField()
-    # total_price = models.DecimalField(max_digits=10, decimal_places=2, blank=True)
+    total_price = models.DecimalField(max_digits=10, decimal_places=2, blank=True)
     status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='Pending')
     order_date = models.DateTimeField(auto_now_add=True)
 
