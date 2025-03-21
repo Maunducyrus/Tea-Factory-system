49788f06d2ed6d3d28be7acc676b26d12d38d447
Maunducyrus <maunducyrus123@gmail.com>
2025-03-21 20:19:00 +0300

    urls

--- tea_factory_backend/api/urls.py
+++ tea_factory_backend/api/urls.py
@@ -17,4 +17,5 @@ urlpatterns = [
     path('login/', login_user, name="login"),
     path('logout/', logout_user, name="logout"),
     path("api/token/", obtain_auth_token, name="api_token_auth"), # Obtain token for authenticated user
+    path('public-order-create/', PublicOrderCreateView.as_view(), name='public-order-create'),
 ]
