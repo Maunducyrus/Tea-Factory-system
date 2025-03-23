c80e22a2b8fa71208244a0c59b5b3359862239fe
Maunducyrus <maunducyrus123@gmail.com>
2025-03-23 17:07:29 +0300

    debugging

--- tea_factory_backend/api/models.py
+++ tea_factory_backend/api/models.py
@@ -11,7 +11,7 @@ class Product(models.Model):
     description = models.TextField()
     price_per_kg = models.DecimalField(max_digits=10, decimal_places=2)
     stock_quantity = models.PositiveIntegerField(default=0)
-    quantity_kg = models.PositiveIntegerField(default=0)
+    # quantity_kg = models.PositiveIntegerField(default=0)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
