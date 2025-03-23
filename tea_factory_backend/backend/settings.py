c0ee0471ea42b6617f3ec26a2697988755dfcf34
Maunducyrus <maunducyrus123@gmail.com>
2025-03-23 18:31:33 +0300

    settings

--- tea_factory_backend/backend/settings.py
+++ tea_factory_backend/backend/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
-import os
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -28,6 +28,9 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+import os
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Application definition
 
@@ -147,5 +150,4 @@ REST_FRAMEWORK = {
     ],
 }
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
