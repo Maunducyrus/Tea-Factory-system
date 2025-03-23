db433074bb2e27564a4b780f08ca9ab46495321d
Maunducyrus <maunducyrus123@gmail.com>
2025-03-23 16:19:09 +0300

    serializers

--- tea_factory_backend/api/serializers.py
+++ tea_factory_backend/api/serializers.py
@@ -16,11 +16,11 @@ class FarmerSerializer(serializers.ModelSerializer):
 # Order Serializer
 class OrderSerializer(serializers.ModelSerializer):
     # changes made - changed product_name to tea_type_name
-    # product_name = serializers.ReadOnlyField(source='product.name', read_only='True')           
+    product_name = serializers.ReadOnlyField(source='product.name', read_only='True')           
     tea_type = serializers.CharField(source='product.tea_type', read_only=True)  # ✅ Get the actual name
     class Meta:
         model = Order
-        fields = ['customer_name', 'phone_number', 'product', 'product_name', 'quantity_kg', 'total_price', 'status', 'order_date']
+        fields = ['customer_name', 'phone_number', 'product', 'product_name', 'tea_type' 'quantity_kg', 'total_price', 'status', 'order_date']
         # fields = '__all__'
 
 # Report Serializer
